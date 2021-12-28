5034fa4d5584f604631f5ce1dda4c8b2b92feb4a
Abayomi Ayodele <samabayomi301@gmail.com>
2021-12-28 21:00:13 +0100

    navbar and routes setup

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -8,15 +8,15 @@ function Navbar() {
     <nav tw='bg-gray-800'>
       <div tw='max-w-7xl mx-auto px-2 sm:(px-6) lg:(px-8)'>
         <div tw='relative flex items-center justify-between h-16'>
-          <div tw='flex-1 flex items-center justify-center sm:(items-stretch justify-start)'>
-            <div tw='flex space-x-6'>
-              <AppLink to='/' aria-current='page'>
-                Home
-              </AppLink>
+          <div tw='flex-1 flex items-center justify-center sm:(items-stretch justify-between)'>
+            <div tw='w-full flex justify-center sm:(justify-between)'>
+              <AppLink to='/'>Home</AppLink>
 
-              <AppLink to='/login'>Login</AppLink>
+              <div tw='flex space-x-6'>
+                <AppLink to='/login'>Login</AppLink>
 
-              <AppLink to='/register'>Register</AppLink>
+                <AppLink to='/register'>Register</AppLink>
+              </div>
             </div>
           </div>
         </div>
--- src/components/nav/Navbar.styled.jsx
+++ src/components/nav/Navbar.styled.jsx
@@ -2,9 +2,13 @@ import { NavLink } from "react-router-dom";
 import tw, { styled } from "twin.macro";
 
 export const AppLink = styled(NavLink)`
-  ${tw`text-gray-300 px-3 py-2 rounded-md text-sm font-medium`}
+  ${tw`
+    text-gray-300 px-3 py-2 rounded-md text-sm font-medium
+    transition-all
+    hover:(bg-gray-700 text-gray-200)
+  `}
 
   &.active {
-    ${tw`bg-gray-900 text-white`}
+    ${tw`bg-gray-900 text-indigo-400`}
   }
 `;
