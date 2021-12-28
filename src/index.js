75aa8b45f366d7eeb8343e4111c0d34917a1c8f8
Abayomi Ayodele <samabayomi301@gmail.com>
2021-12-28 08:11:18 +0100

    twin config

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { ApolloProvider } from "@apollo/client";
 
 import App from "./App";
 import client from "./graphql/client";
+import GlobalStyles from "./styles/GlobalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
+      <GlobalStyles />
       <App />
     </ApolloProvider>
   </React.StrictMode>,
